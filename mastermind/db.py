c95f79f06e641d237011cf8ba79e33be209a975c
Duwang <45719439+WhatABeatifulDuwang@users.noreply.github.com>
2021-06-19 13:56:51 +0200

    save_player half

--- mastermind/db.py
+++ mastermind/db.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+from flask import jsonify
+
 JSON_FILE_PATH = 'mastermind/resources/data.json'
 DEFAULT_DATA = { "players": [
     {
@@ -27,4 +29,20 @@ def get_player(name):
         with open(JSON_FILE_PATH) as file:
             data = json.load(file)
             return next((player_data for player_data in data["players"] if player_data['name'] == name), None)
-            
+
+def save_player(name):
+    jsonFile = open("mastermind/resources/data.json", "r")
+    data = json.load(jsonFile)
+    jsonFile.close()
+
+    data["players"] = {[
+        {
+            "name" = player_data["name"],
+            "games" = [
+                "finished_turn": player_data["finished_turn"],
+                "cheated": player_data["cheated"],
+        ]
+    }
+    }]
+
+            return jsonify([player_data.to_json() for player_data in data])
