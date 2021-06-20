6d9fbcf3d3d254cd315efe47d6200a54dc95b5f5
Jacky <j.zheng657@hotmail.com>
2021-06-20 14:25:34 +0200

    leaderboard sorted by amount played

--- mastermind/controllers/LeaderboardController.py
+++ mastermind/controllers/LeaderboardController.py
@@ -2,6 +2,13 @@ import mastermind
 from flask import render_template, request
 
 def index():
-    mastermind.my_data.all_players = mastermind.db.get_all_players()
+    all_games = []
+    for player in mastermind.db.get_all_players():
+        for game in player['games']:
+            game["player"] = player['name']
+            all_games.append(game)
+    all_games.sort(key=lambda game: game.get('finished_turn'))
+    mastermind.my_data.all_games = all_games
+
     return render_template('leaderboard.html', data=mastermind.my_data)
 
