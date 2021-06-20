cd437d1eb4248a168e09b4c54738e3f1d47dafbb
Jacky <j.zheng657@hotmail.com>
2021-06-20 15:05:52 +0200

    leaderboard finished

--- mastermind/controllers/LeaderboardController.py
+++ mastermind/controllers/LeaderboardController.py
@@ -5,8 +5,10 @@ from flask import render_template
 def index():
     all_games = []
     for player in mastermind.db.get_all_players():
+        amount_games = len(player['games'])
         for game in player['games']:
             game["player"] = player['name']
+            game["amount_games"] = amount_games
             all_games.append(game)
     all_games.sort(key=lambda game: game.get('finished_turn'))
     mastermind.my_data.all_games = all_games
